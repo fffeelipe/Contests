6de7bee67124983e324b1f5fec4d4c2272e0692c
Felipe Epia <fffeelipe@localhost.localdomain>
2018-06-08 18:33:48 -0500

    monsters problem

--- main.cpp
+++ main.cpp
@@ -5,114 +5,48 @@
 #define debug(a) cerr<<#a<<": "<<a<<endl;
 #define fastIO ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 
-typedef long long ll;
+typedef long long unsigned ll;
 using namespace std;
 
-const double PI = atan(1)*4;
-
-    const ll SIZE = 1e6+10;
-    const ll MAX = 1e6+1;
-const ll MOD =1e9+7;
-
-
-bool change(ll &a, ll &b){
-    debug(a)
-            debug(b)
-    if(a>=b*2){
-
-        a-=(b*2)*((a-b*2)/(b*2)+(((a-b*2)%(b*2))?1:0));
-        if(a==b*2)
-            a=0;
-    }else{
-        if(b>= a*2){
-            swap(a,b);
-            a-=(b*2)*((a-b*2)/(b*2)+(((a-b*2)%(b*2))?1:0));
-            swap(a,b);
-            if(b==a*2)
-                b=0;
-        }else{
-            return false;
-        }
+struct Monster
+{
+    ll maxHealth;
+    ll startHealth;
+    ll regen;
+    Monster(ll m=0, ll s=0, ll ren=0) {
+        maxHealth = m;
+        startHealth = s;
+        regen = ren;
     }
-    return true;
-}
+    set<ll> changes;
+};
 
 int main()
 {
 #ifdef FFFeelipe
-    freopen("/home/fffeelipe/Qt Creator projects/Contests/Contests/in.txt" ,"r",stdin);
-    freopen("/home/fffeelipe/Qt Creator projects/Contests/Contests/out.txt","w",stdout);
+    freopen("/home/fffeelipe/Contests/in.txt" ,"r",stdin);
+    freopen("/home/fffeelipe/Contests/out.txt","w",stdout);
 #endif
     fastIO;
-
-    int t;
-    cin>>t;
-    while(t--){
-        string s;
-        cin>>s;
-        string s2=s, sp1 = s.substr(0,s.length()/2) , sp2 = s.substr(s.length()/2,s.length()/2);
-
-        if(s[0]=='1'){
-            bool ba=true;
-            For(i,s.length()-1){
-                if(s[i+1]!='0'){
-                    ba = false;
-                    break;
-                }
-            }
-            if(ba){
-                For(i,s.length()-2)
-                        cout<<9;
-                cout<<endl;
-                continue;
-            }
-
-        }
-
-        string sp1o = sp1;
-        sort(sp1.begin(),sp1.end());
-        For(i,sp1.length()){
-            if(sp1[i]!='0'){
-                swap(sp1[i],sp1[0]);
-            }
-        }
-        if(sp2<=sp1){
-            for(int i = s.length()/2-1;i>=0;i--){
-                if(sp1[i]==0){
-                    sp1[i]=9;
-                }else{
-                    sp1[i]--;
-                    break;
-                }
-            }
-        }
-        map<char, ll> m1;
-        for(char i : sp1){
-            m1[i]++;
-        }
-        bool assi = false;
-        For(i,sp2.length()){
-            if(m1[sp2[i]] && !assi){
-                m1[sp2[i]]--;
-                sp1[i]=sp2[i];
-            }else{
-                assi = true;
-            }
-            if(assi){
-                for(char c = '0';c<='9';c++){
-                    if(m1[c]){
-                        m1[c]--;
-                        sp1[i]=c;
-                    }
-                }
-            }
-        }
-        while(sp2<=sp1){
-            debug(sp1)
-            prev_permutation(sp1.begin(),sp1.end());
-        }
-        cout<<sp1o<<sp1<<'\n';
-
+    ll n, m;
+    cin>>n>>m;
+    ll bounty, increase, damage;
+    cin>>bounty>>increase>>damage;
+    vector<Monster> monsters(3);
+    for(int i = 0; i<n; i++){
+        cin>>monsters[i].maxHealth>>monsters[i].startHealth>>monsters[i].regen;
     }
+    vector<vl> changes;
+    for(int i = 0; i<m;i++){
+        vl temp(3); //time, enemy, health
+        for(j = 0; j<3; j++){
+            cin>>temp[i];
+        }
+        monsters[temp[1]].changes.insert(temp[0]);
+        changes.push_back(temp);
+    }
+
+
+
 
 }
